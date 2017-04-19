47e2f40aa727736cb0de2579924a806c1d4c1979
Harurow <Harurow@users.noreply.github.com>
2017-04-19 14:42:06 +0900

    変数の左右入れ替え機能拡張

--- src/commands/swaps/lr.ts
+++ src/commands/swaps/lr.ts
@@ -1,2 +1,2 @@
 export const swapLr = (str: string) =>
-    str.replace(/^([\t ]*)([a-zA-Z_][a-zA-Z0-9_.]+)([\t ]*[=:][\t ]*)([a-zA-Z_][a-zA-Z0-9_.]+)([\t ]*;?)$/gm, '$1$4$3$2$5')
+    str.replace(/^([\t ]*)([a-zA-Z_][a-zA-Z0-9_.]+)([\t ]*[=:][\t ]*)([a-zA-Z_][a-zA-Z0-9_.]+)([\t ]*[;,]?)$/gm, '$1$4$3$2$5')
